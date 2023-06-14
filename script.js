c7e77ab26440cb32164a8825ab96451b1fc48233
Mohit <102155711+ARYAN4ME@users.noreply.github.com>
2023-06-14 16:21:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -31,12 +31,26 @@ const lookup = {
   ",": ",",
 };
 
-function rot13(encodedStr) {
-  let decodedArr = []; // Your Result goes here
-  // Only change code below this line
+function rot13(encodedString) {
+  var decodedString = '';
 
-  return; //return decodedArr
+  for (var i = 0; i < encodedString.length; i++) {
+    var charCode = encodedString.charCodeAt(i);
+
+    if (charCode >= 65 && charCode <= 90) { // Process uppercase letters only
+      // Apply ROT13 decoding logic
+      var decodedCharCode = ((charCode - 65 + 13) % 26) + 65;
+      var decodedChar = String.fromCharCode(decodedCharCode);
+      decodedString += decodedChar;
+    } else {
+      // Pass non-alphabetic characters as they are
+      decodedString += encodedString.charAt(i);
+    }
+  }
+
+  return decodedString;
 }
+
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
